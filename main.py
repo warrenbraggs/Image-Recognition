578a5524c0734b3b675efc880e968d18f8eaae50
federicobraggs <federicobraggs@outlook.com>
2020-12-28 19:28:02 +0000

    First Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import cv2
+
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+img = cv2.imread("me.jpeg")
+img = cv2.resize(img, (400,400))
+
+
+# Convert the image into a gray colour format (black&white)
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Search the co-ordinates of the image
+faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.1, minNeighbors=3)
+
+
+for x,y,w,h in faces:
+	img = cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0),3)
+
+
+
+
+cv2.imshow("Me", img)
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
