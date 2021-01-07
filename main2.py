b89eb6a483df407417d7872f3a2671537d45263c
federicobraggs <federicobraggs@outlook.com>
2021-01-07 18:37:47 +0000

    Added code for detecting letters

--- main2.py
+++ main2.py
@@ -3,4 +3,32 @@
 import cv2
 import pytesseract 
 
-pytesseract.pytesseract.tesseract 
+#Add the path for tesseract
+pytesseract.pytesseract.tesseract_cmd = r"/usr/local/Cellar/tesseract/4.1.1/bin/tesseract"
+
+#Read the image
+img = cv2.imread("Images/alpha2.png")
+
+#Convert the image
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+# Print the characters detect in the terminal
+print(pytesseract.image_to_string(img, lang='eng',config='--psm 6'))
+
+
+# Get the size of the image
+h_image = img.shape[0]
+w_image = img.shape[1]
+
+# Detect the characters in the image
+boxes = pytesseract.image_to_boxes(img, lang='eng',config='--psm 6')
+
+for i in boxes.splitlines():
+    i = i.split(' ')
+    x,y,w,h = int(i[1]), int(i[2]), int(i[3]), int(i[4])
+    cv2.rectangle(img, (x, h_image-y), (w,h_image-h), (0,255,0),2)
+
+
+#Display the image
+cv2.imshow('main', img)
+cv2.waitKey(0)
