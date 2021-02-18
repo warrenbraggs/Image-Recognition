2ef51a2b153c80712a8a25066947e64bbb71ec4a
federicobraggs <federicobraggs@outlook.com>
2021-02-18 16:40:03 +0000

    Improvements (time complexity)

--- text_detection_live.py
+++ text_detection_live.py
@@ -12,40 +12,46 @@ pytesseract.pytesseract.tesseract_cmd = r"/usr/local/Cellar/tesseract/4.1.1/bin/
 
 
 #Start video capture    (0) -> from webcam
-#                       ('input_your_filename') -> from a file          
+#                       ('input_your_filename') -> from a file        
+
+count = 0
+
 cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
-    ret, img = cap.read()
+    count +=1
 
-    # Convert the image
-    # BGR2BGRA = Real colours
-    # BGR2HSV = For colour detection
-    #t_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    if count%20 == 0:
+        ret, img = cap.read()
 
-    # Detect the most frequent colour in a image
-    b,g,r = (img[300, 300])
-    print (r)
-    print (g)
-    print (b)
+        # Convert the image
+        # BGR2BGRA = Real colours
+        # BGR2HSV = For colour detection
+        #t_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+        # Detect the most frequent colour in a image
+        b,g,r = (img[300, 300])
+        print (r)
+        print (g)
+        print (b)
 
 
-    # Print the characters detected in the terminal
-    print(pytesseract.image_to_string(Image.fromarray(img)))
+        # Print the characters detected in the terminal
+        print(pytesseract.image_to_string(Image.fromarray(img)))
 
-    #
-    #
-    #
-    #
-    #
+        #
+        #
+        #
+        #
+        #
+        
+        
+        #Display the frame
+        cv2.imshow('main', img)
 
-    
-    #Display the frame
-    cv2.imshow('main', img)
-
-    # Use the command ESC to exit
-    if cv2.waitKey(1) == 27 and 0xFF:
-        break
+        # Use the command ESC to exit
+        if cv2.waitKey(1) == 27 and 0xFF:
+            break
 
 cap.release()
 cv2.destroyAllWindows()
